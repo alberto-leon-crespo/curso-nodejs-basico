42c753eae439d89da55fdfb9b0f51bec4f7b32d4
Alberto León Crespo <alberto.leon.crespo@gmail.com>
2020-02-11 23:53:56 +0100

    Base server side rendering

--- src/server.js
+++ src/server.js
@@ -5,7 +5,18 @@ const port = 8080;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-type': 'text/html'});
-    res.write(fs.readFileSync(path.resolve('.', 'src', 'views', 'index.html')));
+    if (req.url === '/' && req.method.toLowerCase() === 'get') {
+        res.write(fs.readFileSync(path.resolve('.', 'src', 'views', 'index.html')));
+    }
+    if (req.url === '/about' && req.method.toLowerCase() === 'get') {
+        res.write(fs.readFileSync(path.resolve('.', 'src', 'views', 'about.html')));
+    }
+    if (req.url === '/services' && req.method.toLowerCase() === 'get') {
+        res.write(fs.readFileSync(path.resolve('.', 'src', 'views', 'services.html')));
+    }
+    if (req.url === '/contact' && req.method.toLowerCase() === 'get') {
+        res.write(fs.readFileSync(path.resolve('.', 'src', 'views', 'contact.html')));
+    }
     res.end();
 }).listen(port);
 console.log('Server listening on port ' + port);
